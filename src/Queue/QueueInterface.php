d0b6cf42d8e03a1fac3014c98ed4e6b9aa72c6a5
ronan-gloo <sonotone13@gmail.com>
2014-08-24 21:27:29 +0200

    Add queue interface

---
+++ src/Queue/QueueInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Qu\Queue;
+
+interface QueueInterface extends \IteratorAggregate, \Countable
+{
+    public function enqueue($message);
+
+    public function requeue($message);
+
+    public function dequeue();
+}
